ac3baff578be9144f9683df709b4a525dd6c635a
Timur Ramazanov <t.ramazanov@idfly.ru>
2016-12-30 11:50:49 +0600

    Add env variable for dev server command options

--- lib/hanami_webpack/dev_server.rb
+++ lib/hanami_webpack/dev_server.rb
@@ -4,7 +4,15 @@ module HanamiWebpack
   module DevServer
     def start
       if HanamiWebpack::Config.using_dev_server? && HanamiWebpack::Config.inbuilt_dev_server?
-        spawn('./node_modules/.bin/webpack-dev-server')
+        command_options = ENV['WEBPACK_DEV_SERVER_CMD_OPTIONS']
+
+        if Hanami::Utils::Blank.blank?(command_options)
+          command_options = Shellwords.escape(command_options)
+        end
+
+        command = "./node_modules/.bin/webpack-dev-server #{command_options}"
+
+        spawn(command.strip)
       end
       super
     end
