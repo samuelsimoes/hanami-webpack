4ea04eada3aa53e34abfe2634fc097aabbbc88bc
Evgeny Garlukovich <evgeny.garlukovich@gmail.com>
2017-10-02 22:00:59 +0300

    Add memoization to HanamiWebpack::Config as ENV-dependent values should not chang

--- lib/hanami_webpack/config.rb
+++ lib/hanami_webpack/config.rb
@@ -1,7 +1,10 @@
 require 'hanami/utils/blank'
+require 'memoizable'
 
 module HanamiWebpack
   class Config
+    include Memoizable
+
     class << self
       extend Forwardable
 
@@ -27,22 +30,27 @@ module HanamiWebpack
     def dev_server_host
       env['WEBPACK_DEV_SERVER_HOST']
     end
+    memoize :dev_server_host
 
     def dev_server_port
       env['WEBPACK_DEV_SERVER_PORT']
     end
+    memoize :dev_server_port
 
     def inbuilt_dev_server?
       env['INBUILT_WEBPACK_DEV_SERVER'] == 'true'
     end
+    memoize :inbuilt_dev_server?
 
     def manifest_file
       env['WEBPACK_MANIFEST_FILE']
     end
+    memoize :manifest_file
 
     def public_path
       env['WEBPACK_PUBLIC_PATH']
     end
+    memoize :public_path
 
     def using_dev_server?
       if Hanami::Utils::Blank.blank?(env['WEBPACK_DEV_SERVER'])
@@ -51,5 +59,6 @@ module HanamiWebpack
         env['WEBPACK_DEV_SERVER'] == 'true'
       end
     end
+    memoize :using_dev_server?
   end
 end
