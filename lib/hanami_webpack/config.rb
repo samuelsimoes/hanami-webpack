80bd5a86776e1213d38a57dfc3b26a211670e933
Radan Skoric <radan.skoric@gmail.com>
2018-10-28 20:27:22 +0100

    Cleanup class interfaces by using eigen class

--- lib/hanami_webpack/config.rb
+++ lib/hanami_webpack/config.rb
@@ -2,32 +2,33 @@ require 'hanami/utils/blank'
 
 module HanamiWebpack
   class Config
+    class << self
+      def public_path
+        ENV['WEBPACK_PUBLIC_PATH']
+      end
 
-    def self.public_path
-      ENV['WEBPACK_PUBLIC_PATH']
-    end
+      def manifest_file
+        ENV['WEBPACK_MANIFEST_FILE']
+      end
 
-    def self.manifest_file
-      ENV['WEBPACK_MANIFEST_FILE']
-    end
+      def dev_server_host
+        ENV['WEBPACK_DEV_SERVER_HOST']
+      end
 
-    def self.dev_server_host
-      ENV['WEBPACK_DEV_SERVER_HOST']
-    end
+      def dev_server_port
+        ENV['WEBPACK_DEV_SERVER_PORT']
+      end
 
-    def self.dev_server_port
-      ENV['WEBPACK_DEV_SERVER_PORT']
-    end
+      def inbuilt_dev_server?
+        ENV['INBUILT_WEBPACK_DEV_SERVER'] == 'true'
+      end
 
-    def self.inbuilt_dev_server?
-      ENV['INBUILT_WEBPACK_DEV_SERVER'] == 'true'
-    end
-
-    def self.using_dev_server?
-      if Hanami::Utils::Blank.blank?(ENV['WEBPACK_DEV_SERVER'])
-        ENV['RACK_ENV'] != 'production'
-      else
-        ENV['WEBPACK_DEV_SERVER'] == 'true'
+      def using_dev_server?
+        if Hanami::Utils::Blank.blank?(ENV['WEBPACK_DEV_SERVER'])
+          ENV['RACK_ENV'] != 'production'
+        else
+          ENV['WEBPACK_DEV_SERVER'] == 'true'
+        end
       end
     end
   end
--- lib/hanami_webpack/manifest.rb
+++ lib/hanami_webpack/manifest.rb
@@ -6,58 +6,62 @@ require_relative 'entry_point_missing_error'
 
 module HanamiWebpack
   class Manifest
-    def self.bundle_uri(bundle_name)
+    class << self
+      def bundle_uri(bundle_name)
 
-      raise HanamiWebpack::WebpackError, manifest['errors'] unless Hanami::Utils::Blank.blank?(manifest['errors'])
+        raise HanamiWebpack::WebpackError, manifest['errors'] unless Hanami::Utils::Blank.blank?(manifest['errors'])
 
-      path = manifest[bundle_name] || manifest['assetsByChunkName'][bundle_name]
+        path = manifest[bundle_name] || manifest['assetsByChunkName'][bundle_name]
 
-      if Hanami::Utils::Blank.blank?(path)
-        raise HanamiWebpack::EntryPointMissingError, "Can't find entry point '#{bundle_name}' in webpack manifest"
+        if Hanami::Utils::Blank.blank?(path)
+          raise HanamiWebpack::EntryPointMissingError, "Can't find entry point '#{bundle_name}' in webpack manifest"
+        end
+
+        path = public_path(path)
+
+        if HanamiWebpack::Config.using_dev_server?
+          path = "//#{HanamiWebpack::Config.dev_server_host}:#{HanamiWebpack::Config.dev_server_port}#{path}"
+        end
+
+        path
       end
 
-      path = public_path(path)
+      private
 
-      if HanamiWebpack::Config.using_dev_server?
-        path = "//#{HanamiWebpack::Config.dev_server_host}:#{HanamiWebpack::Config.dev_server_port}#{path}"
+      def manifest_path
+        public_path(HanamiWebpack::Config.manifest_file)
       end
 
-      path
-    end
-
-    def self.manifest_path
-      public_path(HanamiWebpack::Config.manifest_file)
-    end
-
-    def self.public_path(relative_path)
-      Hanami::Utils::PathPrefix.new('/').join(HanamiWebpack::Config.public_path).join(relative_path)
-    end
-
-    def self.remote_manifest
-      host = HanamiWebpack::Config.dev_server_host
-      port = HanamiWebpack::Config.dev_server_port
-      http = Net::HTTP.new(host, port)
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      response = http.get(manifest_path).body
-      JSON.parse(response)
-    end
-
-    def self.static_manifest
-      @_manifest ||= begin
-        path =
-          Hanami
-            .public_directory
-            .join(*HanamiWebpack::Config.public_path.split('/'))
-            .join(HanamiWebpack::Config.manifest_file)
-
-        file = File.read(path)
-
-        JSON.parse(file)
+      def public_path(relative_path)
+        Hanami::Utils::PathPrefix.new('/').join(HanamiWebpack::Config.public_path).join(relative_path)
       end
-    end
 
-    def self.manifest
-      HanamiWebpack::Config.using_dev_server? ? remote_manifest : static_manifest
+      def remote_manifest
+        host = HanamiWebpack::Config.dev_server_host
+        port = HanamiWebpack::Config.dev_server_port
+        http = Net::HTTP.new(host, port)
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        response = http.get(manifest_path).body
+        JSON.parse(response)
+      end
+
+      def static_manifest
+        @_manifest ||= begin
+          path =
+            Hanami
+              .public_directory
+              .join(*HanamiWebpack::Config.public_path.split('/'))
+              .join(HanamiWebpack::Config.manifest_file)
+
+          file = File.read(path)
+
+          JSON.parse(file)
+        end
+      end
+
+      def manifest
+        HanamiWebpack::Config.using_dev_server? ? remote_manifest : static_manifest
+      end
     end
   end
 end
